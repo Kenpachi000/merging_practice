d1c6654403c25f7ba67c0c31d032f590c640720c
Gavin <gavinwu23@gmail.com>
2020-08-19 21:35:08 -0500

    new branch

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+    // hello this is a test
+
+    public static int addNumbers(int a, int b) {
+        return -100;
+    }
+
+    public void newMethodFromGen( ) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
