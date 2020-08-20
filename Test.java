84b125c7c3f80620c3018065d9f164d86ca4eeb3
Gavin <gavinwu23@gmail.com>
2020-08-19 22:16:52 -0500

    merging

---
+++ Test.java
@@ -0,0 +1,31 @@
+public class Test {
+
+    // hello this is a test
+
+    public static int addNumbers(int jimmy, int johns) {
+                // store the 2 numbers from parameter and then add them and return them
+        return 600;
+
+        // vroom vroom
+    }
+
+    public void newMethodFromGen( ) {
+
+        // branch branch
+        //hello
+        return;
+    }
+
+    public void subtract( ) {
+        // this is from subtract brannch
+        // subtract branch
+    }
+
+    public void lebronjames ( ) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("fuck me");
+    }
+}
