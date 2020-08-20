986ff9144198106a9684e838762a710efa98adc5
Gavin <gavinwu23@gmail.com>
2020-08-19 20:58:38 -0500

    finished

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+
+    public static int addNumbers(int a, int b) {
+
+        // store the 2 numbers from parameter and then add them and return them
+        int x = a;
+        int y = b;
+        return x+y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
